d190c1cad417f5cc6c9c050196222f23a5e87225
Johnny <zarrijohnny70@gmail.com>
2019-05-24 10:57:49 +0200

    first commit

---
+++ Backup/index2 - Copie.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Doc</title>
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="./lib/jquery.min.js"></script> 
+    <script type="text/javascript" src="./lib/jquery-ui.js"></script>
+    <script type="text/javascript">
+   // Code inline
+    </script>
+    
+</head>   
+   
+<body>
+<?php 
+?>
+    <h1>Geneva Business News Services: </h1>
+   
+    <table>
+        <tbody>
+            
+            <ul>
+               <li><a id="" class="" href="glpi/">Gestion libre de parc Informatique</a></li>
+               <li><a id="" class="" href="ocs/">Ocs Inventory</a></li>
+               <li><a id="" class="" href="tikiwiki/">TikiWiki</a></li>
+            
+            </ul>     
+                
+        </tbody>
+    </table>
+ 
+</body>
+</html>
+
+  
+
---
+++ Backup/index2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Doc</title>
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="./lib/jquery.min.js"></script> 
+    <script type="text/javascript" src="./lib/jquery-ui.js"></script>
+    <script type="text/javascript">
+    <script>
+    
+   $( function() {
+    $( "#menu" ).menu();
+  } );
+   </script>
+   <style>
+   .ui-menu { width: 150px; }
+   </style>
+</head>   
+<body>
+<?php 
+?>
+    <h1>Geneva Business News Services: </h1>
+   
+    <table>
+        <tbody>
+            
+            <ul id="menu">
+               <li><div><a id="" class="" href="glpi/">Gestion libre de parc Informatique</a></div></li>
+               <li><div><a id="" class="" href="ocs/">Ocs Inventory</a></div></li>
+               <li><div><a id="" class="" href="tikiwiki/">TikiWiki</a></div></li>
+            
+            </ul>     
+                
+        </tbody>
+    </table>
+ 
+</body>
+</html>
+
+  
+
